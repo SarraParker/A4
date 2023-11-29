6febd3dd760d891bd8b130cfeccf587b64058f0f
Sarra Parker <sarra@uoguelph.ca>
2023-11-28 20:57:17 -0500

    Print key edits

--- aa_mainline.c
+++ aa_mainline.c
@@ -185,7 +185,7 @@ printIteratorValue(AAKeyType key, size_t keylen, void *value, void *userdata)
 	char keybuffer[LINE_MAX];	// we will simply truncate keys at this length
 	FILE *ofp = (FILE *) userdata;
 
-	if (printableKey(keybuffer, LINE_MAX, key, keylen) < 0) {
+	if (myPrintableKey(keybuffer, LINE_MAX, key, keylen) < 0) {
 		fprintf(stderr, "Error: key conversion failed!");
 		return -1;
 	}
@@ -302,6 +302,7 @@ main(int argc, char **argv)
 			}
 
 		} else if (c == 'h') {
+			
 			usage(programname);
 		}
 	}
